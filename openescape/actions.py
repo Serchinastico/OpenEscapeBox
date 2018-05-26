88ec4c397de82c7c285de2a7c369001cebc78835
Sergio Gutierrez <sergio@gokarumi.com>
2018-05-26 14:27:25 +0200

    Merge with carlos' work

--- openescape/actions.py
+++ openescape/actions.py
@@ -1,11 +1,18 @@
+import serial
+import time
+import os
+
+
 class Action(object):
-	def execute(self):
-		pass
+    def execute(self):
+        pass
+
 
 class TurnLightOnAction(Action):
-	def __init__(self, config):
-		# TODO Add real dependencies
-		pass
+    def __init__(self, config):
+        self.connection = serial.Serial('/dev/ttyACM0', 57600)
+        pass
 
-	def execute(self):
-		print('Turn light on')
+    def execute(self):
+        self.connection.write("d13=1")
+        self.connection.flush()
